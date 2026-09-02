760785b0cb99215e0e79eb22f5fceaa75a3ddcae
Matex0406 <mateeuscortez2006@gmail.com>
2026-09-02 19:30:22 -0300

    Planilha csv e uma organizada pra análise visual

--- Analise_Airbnb.py
+++ Analise_Airbnb.py
@@ -1,6 +1,11 @@
 import kagglehub
 
+
+'''
 # Download latest version
 path = kagglehub.dataset_download("ashishjangra27/airbnb-dataset")
 
 print("Path to dataset files:", path)
+'''
+
+print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
